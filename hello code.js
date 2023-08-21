9da64de7f8843a9393d5af6abbce57e227fbdcf1
bitrsweet29 <142755859+bitrsweet29@users.noreply.github.com>
2023-08-20 22:30:55 -0500

    Add files via upload

---
+++ hello code.js
@@ -0,0 +1,13 @@
+/*  
+Welcome, new coder. 
+Enter your code on line 7 to print 'Hello, World!' 
+to the console (the panel on the right)
+*/
+
+console.log('This is so cool!')
+console.log("I wasnt sure I would be able to figure this out BUT...")
+console.log("Here I am writing code and Im absolutly ecstatic!")
+console.log("Yay me!! I knew I would get it eventually!!")
+
+
+
